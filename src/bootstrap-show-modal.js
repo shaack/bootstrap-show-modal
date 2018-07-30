a060d45a30ab756949b3a0992ff46e72c522d392
Stefan Haack <shaack@gmail.com>
2018-07-30 10:47:21 +0200

    moved the polyfill to the bottom

--- src/bootstrap-show-modal.js
+++ src/bootstrap-show-modal.js
@@ -7,34 +7,7 @@
 (function ($) {
     "use strict"
 
-    if (typeof Object.assign !== 'function') {
-        Object.defineProperty(Object, "assign", {
-            value: function assign(target, varArgs) { // .length of function is 2
-                if (target == null) { // TypeError if undefined or null
-                    throw new TypeError('Cannot convert undefined or null to object')
-                }
-                var to = Object(target)
-                for (var index = 1; index < arguments.length; index++) {
-                    var nextSource = arguments[index]
-
-                    if (nextSource != null) { // Skip over if undefined or null
-                        for (var nextKey in nextSource) {
-                            // Avoid bugs when hasOwnProperty is shadowed
-                            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-                                to[nextKey] = nextSource[nextKey]
-                            }
-                        }
-                    }
-                }
-                return to
-            },
-            writable: true,
-            configurable: true
-        })
-    }
-
     var i = 0
-
     function Modal(props) {
         this.props = {
             modalClass: "", // Additional css for ".modal"
@@ -145,5 +118,31 @@
             return this.showModal(props, postCreate, callback)
         }
     })
+
+    // polyfill for Object.assign
+    if (typeof Object.assign !== 'function') {
+        Object.defineProperty(Object, "assign", {
+            value: function assign(target, varArgs) {
+                if (target == null) {
+                    throw new TypeError('Cannot convert undefined or null to object')
+                }
+                var to = Object(target)
+                for (var index = 1; index < arguments.length; index++) {
+                    var nextSource = arguments[index]
+
+                    if (nextSource != null) {
+                        for (var nextKey in nextSource) {
+                            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                                to[nextKey] = nextSource[nextKey]
+                            }
+                        }
+                    }
+                }
+                return to
+            },
+            writable: true,
+            configurable: true
+        })
+    }
 
 }(jQuery))
