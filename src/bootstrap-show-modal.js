5c99a0ca2d1d2581da627b367f662ecc7176478b
Stefan Haack <shaack@gmail.com>
2019-01-07 23:53:49 +0100

    Better IE support, without polyfill. 25 lines less code. :)

--- src/bootstrap-show-modal.js
+++ src/bootstrap-show-modal.js
@@ -21,7 +21,9 @@
             onDispose: null, // Callback, called after the modal was disposed
             onSubmit: null // Callback of $.showConfirm(), called after yes or no was pressed
         }
-        Object.assign(this.props, props)
+        for (var prop in props) {
+            this.props[prop] = props[prop]
+        }
         this.id = "bootstrap-show-modal-" + i
         i++
         this.show()
@@ -122,31 +124,5 @@
             return this.showModal(props)
         }
     })
-
-    // polyfill for Object.assign
-    if (typeof Object.assign !== 'function') {
-        Object.defineProperty(Object, "assign", {
-            value: function assign(target, varArgs) {
-                if (target == null) {
-                    throw new TypeError('Cannot convert undefined or null to object')
-                }
-                var to = Object(target)
-                for (var index = 1; index < arguments.length; index++) {
-                    var nextSource = arguments[index]
-
-                    if (nextSource != null) {
-                        for (var nextKey in nextSource) {
-                            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-                                to[nextKey] = nextSource[nextKey]
-                            }
-                        }
-                    }
-                }
-                return to
-            },
-            writable: true,
-            configurable: true
-        })
-    }
 
 }(jQuery))
